108d41f81a9ce5e456fbb0374efc842566e29458
Elyor Latipov <elyor.blog@gmail.com>
2018-02-22 23:34:18 +0500

    To fix client/serv commonucation data transfer

--- standart/TelnetCalc.Client/Program.cs
+++ standart/TelnetCalc.Client/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Globalization;
 using System.Threading;
+using Hik.Communication.Scs.Client;
 using Hik.Communication.Scs.Communication.EndPoints.Tcp;
+using Hik.Communication.Scs.Communication.Messages;
 using Hik.Communication.ScsServices.Client;
-using TelnetCalc.Common;
 
 namespace TelnetCalc.Client
 {
@@ -14,14 +15,22 @@ namespace TelnetCalc.Client
 
             //Create a client that can call methods of Calculator Service that is running on local computer and 10083 TCP port
             var port = 10083;
-            
+            var host = "127.0.0.1";
+
             //Since IScsServiceClient is IDisposible, it closes connection at the end of the using block
-            using (var client = ScsServiceClientBuilder.CreateClient<ICalculatorService>(new ScsTcpEndPoint("127.0.0.1", port)))
+            using (var client = ScsClientFactory.CreateClient(new ScsTcpEndPoint(host, port)))
             {
                 //Connect to the server
                 client.Connect();
-                Console.WriteLine($"Server is connected by {port} port");
+                client.MessageReceived += Client_MessageReceived;
 
+                //console
+                Console.Title = $"Client {host}:{port}";
+                Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+                {
+                    client.Disconnect();
+                };
+                Console.WriteLine($"Server is connected by {port} port");
                 Console.WriteLine("Press enter to stop client application");
 
                 do
@@ -35,34 +44,42 @@ namespace TelnetCalc.Client
                         {
                             client.Disconnect();
                             Console.WriteLine($"Server is disconnected by {port} port");
-                        
+
                             break;
                         }
 
                         if (!int.TryParse(s, NumberStyles.None, null, out var n))
                         {
                             Console.WriteLine("Incorrect format,please try again!");
-                        
+
                             continue;
                         }
-
-                         var sum=  client.ServiceProxy.Add(n);
-
-                        Console.WriteLine($"Sum: {sum}");
-
+                         
+                        client.SendMessage(new ScsTextMessage(s));
+                         
                     }
                     catch (Exception e)
                     {
-                        Console.WriteLine(e); 
+                        Console.WriteLine(e);
                     }
-                    
+
                     Thread.Sleep(100);
-                    
+
                 } while (true);
 
             }
 
-            Console.ReadKey(); 
+            Console.ReadKey();
+        }
+
+        private static void Client_MessageReceived  (object sender, MessageEventArgs e)
+        {
+            var message = (ScsTextMessage)e.Message;
+
+            if (!string.IsNullOrWhiteSpace(message.RepliedMessageId))
+            {
+                Console.WriteLine(message.Text);
+            }
         }
     }
 }
--- standart/TelnetCalc.Server/Program.cs
+++ standart/TelnetCalc.Server/Program.cs
@@ -1,57 +1,117 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
 using Hik.Communication.Scs.Communication.EndPoints.Tcp;
+using Hik.Communication.Scs.Communication.Messages;
+using Hik.Communication.Scs.Server;
 using Hik.Communication.ScsServices.Service;
-using TelnetCalc.Common;
 
 namespace TelnetCalc.Server
 {
     internal static class Program
     {
+        private readonly static IDictionary<long, int> _numbers = new Dictionary<long, int>();
+
         private static void Main(string[] args)
         {
             var port = 10083;
-            
-            //Create a service application that runs on 10083 TCP port
-            var serviceApplication = ScsServiceBuilder.CreateService(new ScsTcpEndPoint(port));
 
-            //Create a CalculatorService and add it to service application
-            serviceApplication.AddService<ICalculatorService, CalculatorService>(new CalculatorService());
-            
+            //Create a service application that runs on 10083 TCP port
+            var server = ScsServerFactory.CreateServer(new ScsTcpEndPoint(port));
+
             //Start service application
-            serviceApplication.Start();
-            Console.WriteLine($"Calculator service is started by {port}");
-            
+            server.Start();
+
             //events
-            serviceApplication.ClientConnected+=ServiceApplicationOnClientConnected;
-            serviceApplication.ClientDisconnected+=ServiceApplicationOnClientDisconnected;
-            
-            Console.WriteLine("Press enter to stop...");
-            Console.ReadLine();
+            server.ClientConnected += Server_ClientConnected;
+            server.ClientDisconnected += Server_ClientDisconnected;
+             
+            //console
+            Console.Title = $"Server 127.0.0.1:{port}";
+            Console.WriteLine($"Server is connected by {port} port");
+            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+            {
+                server.Stop();
+            };
+            Console.WriteLine("Press enter to stop server application");
+
+            while (true)
+            {
+                var s = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(s))
+                    break;
+
+                Thread.Sleep(100);
+            }
 
             //Stop service application
-            serviceApplication.RemoveService<ICalculatorService>();
-            serviceApplication.Stop();
-            
+            server.Stop();
+
         }
 
-        private static void ServiceApplicationOnClientDisconnected(object sender, ServiceClientEventArgs serviceClientEventArgs)
+        private static void Server_ClientDisconnected(object sender, ServerClientEventArgs e)
         {
-            var serv = serviceClientEventArgs.Client.GetClientProxy<ICalculatorService>();
-            serv.Init();
+            var client = (IScsServerClient)sender;
+            var id = client.ClientId;
+
+            if (_numbers.ContainsKey(id))
+                _numbers.Remove(id);
 
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Client#{serviceClientEventArgs.Client.ClientId} is disconnected");
+            Console.WriteLine($"Client#{e.Client.ClientId} is disconnected");
             Console.ResetColor();
+
         }
 
-        private static void ServiceApplicationOnClientConnected(object sender, ServiceClientEventArgs serviceClientEventArgs)
+        private static void Server_ClientConnected(object sender, ServerClientEventArgs e)
         {
-            var serv = serviceClientEventArgs.Client.GetClientProxy<ICalculatorService>();
-            serv.Clear();
-            
+            var id = e.Client.ClientId;
+
+            if (!_numbers.ContainsKey(id))
+                _numbers.Add(id, 0);
+
+            e.Client.MessageReceived += Client_MessageReceived;
+            e.Client.MessageSent += Client_MessageSent;
+
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Client#{serviceClientEventArgs.Client.ClientId} is connected");
+            Console.WriteLine($"Client#{e.Client.ClientId} is connected. Address: " + e.Client.RemoteEndPoint);
             Console.ResetColor();
+
+        }
+
+        private static void Client_MessageSent(object sender, MessageEventArgs e)
+        {
+            var message = (ScsTextMessage)e.Message;
+            var client = (IScsServerClient)sender;
+            var id = client.ClientId;
+
+            Console.WriteLine($"Client#{id}: {message.Text}");
+        }
+
+        private static void Client_MessageReceived(object sender, MessageEventArgs e)
+        {
+            var message = (ScsTextMessage)e.Message;
+            var client = (IScsServerClient)sender;
+            var id = client.ClientId;
+
+            if (int.TryParse(message.Text, NumberStyles.None, null, out var n))
+            {
+                _numbers[id] += n;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------------");
+            Console.WriteLine("Total sum: {0}", _numbers.Sum(a => a.Value));
+            Console.WriteLine();
+
+            client.SendMessage(new ScsTextMessage("Total sum: " + _numbers[id])
+            {
+                RepliedMessageId = Guid.NewGuid().ToString()
+            });
         }
     }
 }
